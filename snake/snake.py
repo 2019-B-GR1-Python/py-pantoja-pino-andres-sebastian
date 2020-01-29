05dfb2eb6a3f71c2bff7094d4d338c4e9329de88
ANDRES SEBASTIAN PANTOJA PINO <andres.pantoja@epn.edu.ec>
2020-01-28 20:30:06 -0500

    Update snake.py

--- snake/snake.py
+++ snake/snake.py
@@ -1,26 +1,85 @@
 import pygame, sys
 from pygame.locals import *
+import time
+from random import randint
 
-class Serpiente:
-    posicion_en_x = 10
-    posicion_en_y = 10
-    velocidad = 1
+class Comida:
+    x = 0
+    y = 0
+    paso = 44
     
+    def __init__(self, x, y):
+        self.x = x * self.paso
+        self.y = y * self.paso
+
+    def dibujar(self, superficie, imagen):
+        superficie.blit(imagen, (self.x, self.y))
+        
+        
+class Serpiente:
+    x = [0]
+    y = [0]
+    paso = 44
+    direccion = 0
+    longitud = 3
+    
+    cuenta_maxima = 2
+    cuenta = 0
+    
+    def __init__(self, longitud):
+        self.longitud = longitud
+        for i in range(0, 2000):
+            self.x.append(-100)
+            self.y.append(-100)
+            
+        self.x[1] = 1 * 44
+        self.x[2] = 2 * 44
+        
+    
+    def actualizar(self):
+        self.cuenta = self.cuenta + 1
+        if self.cuenta > self.cuenta_maxima:
+            
+            for i in range(self.longitud - 1, 0, -1):
+                self.x[i] = self.x[i - 1]
+                self.y[i] = self.y[i - 1]
+        
+            
+            if self.direccion == 0:
+                self.x[0] = self.x[0] + self.paso
+            if self.direccion == 1:
+                self.x[0] = self.x[0] - self.paso
+            if self.direccion == 2:
+                self.y[0] = self.y[0] + self.paso
+            if self.direccion == 3:
+                self.y[0] = self.y[0] - self.paso
+        
+            self.cuenta = 0
+            
+            
     def mover_izquierda(self):
-        self.posicion_en_x = self.posicion_en_x - self.velocidad
+        self.direccion = 0
 
     def mover_derecha(self):
-        self.posicion_en_x = self.posicion_en_x + self.velocidad
+        self.direccion = 1
         
     def mover_arriba(self):
-        self.posicion_en_y = self.posicion_en_y - self.velocidad
+        self.direccion = 2
         
     def mover_abajo(self):
-        self.posicion_en_y = self.posicion_en_y + self.velocidad
+        self.direccion = 3
 
+    def dibujar(self, superficie, imagen):
+        for i in range(0, self.longitud):    
+            superficie.blit(imagen, (self.x[i], self.y[i]))
+        
 
-
-
+class Colision:
+    def hay_colision(self, x1, y1, x2, y2, box_size):
+        if x1 >= x2 and x1 <= (x2 + box_size):
+            if y1 >= y2 and y1 <= (y2 + box_size):
+                return True
+        return False
     
 
 class Juego:
@@ -31,9 +90,15 @@ class Juego:
     display = None
     imagen = None
     nombre_imagen = "serpiente.png"
+    imagen_comida = None
+    comida = None
     def __init__(self):
         self.en_juego = True
-        self.serpiente = Serpiente()
+        self.serpiente = Serpiente(3)
+        self.comida = Comida(5, 5)
+        self.colision = Colision()
+        
+        
         
     def on_init(self):
         pygame.init()
@@ -42,18 +107,45 @@ class Juego:
         pygame.display.set_caption('Mi juiguito de snake')
         self.en_juego = True
         self.imagen = pygame.image.load(self.nombre_imagen).convert()
+        self.imagen_comida = pygame.image.load(self.nombre_imagen).convert()
         
         
     def espera(self):
+        self.serpiente.actualizar()
+        
+        for i in range(0, self.serpiente.longitud):
+            if self.colision.hay_colision(
+                    self.comida.x,
+                    self.comida.y,
+                    self.serpiente.x[i],
+                    self.serpiente.y[i],
+                    44):
+                self.comida.x = randint(2, 9) * 44
+                self.comida.y = randint(2, 9) * 44
+                self.serpiente.longitud = self.serpiente.longitud + 1 
+        
+        
+        for i in range(2, self.serpiente.longitud):
+            if self.colision.hay_colision(
+                    self.serpiente.x[0],
+                    self.serpiente.y[0],
+                    self.serpiente.x[i],
+                    self.serpiente.y[i],
+                    40):
+                print("Colision!")
+                exit(0)
         pass
     
+        
     def on_event(self, event):
         if event.type == QUIT:
             self.en_juego = False
             
+            
     def on_render(self):
         self.display.fill((0, 0, 0))
-        self.display.blit(self.imagen, (self.serpiente.posicion_en_x, self.serpiente.posicion_en_y))
+        self.serpiente.dibujar(self.display, self.imagen)
+        self.serpiente.dibujar(self.display, self.imagen_comida)
         pygame.display.flip()
         
     def on_cleanup(self):
@@ -79,9 +171,10 @@ class Juego:
             if (teclas[K_DOWN]):
                 self.serpiente.mover_abajo()
                 
-            self.espera()
             self.on_render()
-        
+            self.espera()
+            time.sleep (50.0 / 1000.0);
+            
         self.on_cleanup()
     
     
